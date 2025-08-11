0311806ed67d22c46c8e21b52788402ce3a43973
MrKssksksksk <mrkssksksksk@gmail.com>
2025-08-11 18:49:15 +0800

    Update to new unity version AND create start scene

---
+++ Kards Tank/Assets/StartSceneScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public void loadMainScene()
+    {
+        // Scene currentScene = SceneManager.GetActiveScene();
+        Debug.Log("button pressed.");
+        SceneManager.LoadScene(1);
+    }
+}
