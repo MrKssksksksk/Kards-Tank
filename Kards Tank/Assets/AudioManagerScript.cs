7c6af789cad7bdb8d51def627cd61503a8f03eeb
SaltyFive <xxccyy_aa@163.com>
2025-08-14 21:06:01 +0800

    D日资源

--- Kards Tank/Assets/AudioManagerScript.cs
+++ Kards Tank/Assets/AudioManagerScript.cs
@@ -1,38 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class AudioManagerScript : MonoBehaviour
 {
-    [SerializeField] AudioSource BgmAudio;
-    //public AudioClip japBGM;
-    //public AudioClip gerBGM;
-    //public AudioClip sovBGM;
-    //public AudioClip engBGM;
-    //public AudioClip usaBGM;
+    [SerializeField] AudioSource BgmAudio; //在Unity编辑器查看音效列表
     public List<AudioClip> bgmList = new List<AudioClip>();
 
     [SerializeField] AudioSource SfxAudio; //Sfx:音效
-
-    //public AudioClip MediumTankMove; //MT中型坦克 HT重型坦克
-    //public AudioClip MediumTankAttack;
-    //public AudioClip HeavyTankMove;
-    //public AudioClip HeavyTankAttack;
-
-    //public AudioClip DecreaseActionCost; //减行动花费
-    //public AudioClip Trigger;  //齿轮触发
-
-    //public AudioClip sovLashen;
-    //public AudioClip sovSu; //转折点
-
-    //public AudioClip japTaiko2;  //Taiko：太鼓 2是打两下
-    //public AudioClip japSurpriseAttack; //偷袭
-
-    //public AudioClip gerMillitaryMusic; //军乐
-    //public AudioClip gerBlitzDoctrine; //闪击战法
-    //public AudioClip gerContestDoctrine; //竞争战法
-
-    //public AudioClip engNavalPower; //海军力量
 
     public List<AudioClip> sfxList = new List<AudioClip>();
 
@@ -49,8 +26,9 @@ public class AudioManagerScript : MonoBehaviour
 
     void Start()
     {
+        int bgmIndex = Random.Range(0, bgmList.Count());
         PlaySfx(16); // 回合切换
-        PlayBgm(0); // jap bgm
+        PlayBgm(bgmIndex); // jap bgm
     }
 
 }
